d74d0b49b09fe2b85bf3dc8cd9f66a6d21cea10a
★. KuJu -TH- <plan291037@outlook.com>
2023-02-04 17:34:04 +0700

    Update guildService.js

--- src/guildService.js
+++ src/guildService.js
@@ -1,106 +1,117 @@
-const persistance = require('./persistance')
-const { Guild } = require('./Guild')
+const persistance = require("./persistance");
+const { Guild } = require("./Guild");
 
 const defaultServer = {
-    "preJoinTimer": 900,
-    "warChannel": "war-channel",
-    "callRate": [15, 10, 5],
-    "firstCallTimer": 120,
-    "warCount": 0,
-    "timeZone": 0
-}
+  preJoinTimer: 900,
+  warChannel: "war-channel",
+  callRate: [15, 10, 5],
+  firstCallTimer: 60,
+  warCount: 0,
+  timeZone: 0,
+};
 
 function createDefault(id) {
-    server = { ...defaultServer }
-    server["id"] = id
-    return new Promise((resolve, reject) => {
-        persistance.create(server).then((data, err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            const tmp = new Guild(data.id, data.preJoinTimer, data.warChannel, data.callRate, data.warCount, data.timeZone)
-            resolve(tmp);
-        })
-    })
+  server = { ...defaultServer };
+  server["id"] = id;
+  return new Promise((resolve, reject) => {
+    persistance.create(server).then((data, err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const tmp = new Guild(
+        data.id,
+        data.preJoinTimer,
+        data.warChannel,
+        data.callRate,
+        data.warCount,
+        data.timeZone
+      );
+      resolve(tmp);
+    });
+  });
 }
 
 function deleteServer(id) {
-    persistance.deleteServer(id);
+  persistance.deleteServer(id);
 }
 
 function save(guild) {
-    const obj = {
-        "id": guild.id,
-        "preJoinTimer": guild.preJoinTimer,
-        "warChannel": guild.warChannel,
-        "callRate": guild.callRate,
-        "firstCallTimer": guild.firstCallTimer,
-        "warCount": guild.warCount,
-        "timeZone": guild.timeZone
-    }
-    return persistance.save(obj);
+  const obj = {
+    id: guild.id,
+    preJoinTimer: guild.preJoinTimer,
+    warChannel: guild.warChannel,
+    callRate: guild.callRate,
+    firstCallTimer: guild.firstCallTimer,
+    warCount: guild.warCount,
+    timeZone: guild.timeZone,
+  };
+  return persistance.save(obj);
 }
 
 const idToServerMap = new Map();
 
 function get(id) {
-    return new Promise((resolve, reject) => {
-        persistance.find(id).then((data, err) => {
-            if (err) {
-                reject(err);
-            } else {
-                if (!data) {
-                    resolve(data);
-                    return;
-                }
-                const tmp = new Guild(data.id, data.preJoinTimer, data.warChannel, data.callRate, data.firstCallTimer, data.warCount, data.timeZone)
-                resolve(tmp);
-            }
-        })
-    })
+  return new Promise((resolve, reject) => {
+    persistance.find(id).then((data, err) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (!data) {
+          resolve(data);
+          return;
+        }
+        const tmp = new Guild(
+          data.id,
+          data.preJoinTimer,
+          data.warChannel,
+          data.callRate,
+          data.firstCallTimer,
+          data.warCount,
+          data.timeZone
+        );
+        resolve(tmp);
+      }
+    });
+  });
 }
 
-
-
 function getOrCreate(id) {
-    return new Promise(resolve => {
-        if (!idToServerMap.has(id)) {
-            get(id).then(resultGuild => {
-                if (resultGuild) {
-                    idToServerMap.set(id, resultGuild)
-                    resolve(idToServerMap.get(id));
-                } else {
-                    createDefault(id).then((resultDefault) => {
-                        idToServerMap.set(id, resultDefault);
-                        resolve(idToServerMap.get(id));
-                    })
-                }
-            })
+  return new Promise((resolve) => {
+    if (!idToServerMap.has(id)) {
+      get(id).then((resultGuild) => {
+        if (resultGuild) {
+          idToServerMap.set(id, resultGuild);
+          resolve(idToServerMap.get(id));
         } else {
+          createDefault(id).then((resultDefault) => {
+            idToServerMap.set(id, resultDefault);
             resolve(idToServerMap.get(id));
+          });
         }
-    })
-
+      });
+    } else {
+      resolve(idToServerMap.get(id));
+    }
+  });
 }
 
 function interact(id, interaction) {
-    if (!idToServerMap.has(id)) {
-        get(id).then(resultGuild => {
-            if (resultGuild) {
-                idToServerMap.set(id, resultGuild)
-                idToServerMap.get(id).interact(interaction);
-            } else {
-                createDefault(id).then((resultDefault) => {
-                    idToServerMap.set(id, resultDefault);
-                    idToServerMap.get(id).interact(interaction);
-                })
-            }
-        })
-    } else {
+  if (!idToServerMap.has(id)) {
+    get(id).then((resultGuild) => {
+      if (resultGuild) {
+        idToServerMap.set(id, resultGuild);
         idToServerMap.get(id).interact(interaction);
-    }
-
+      } else {
+        createDefault(id).then((resultDefault) => {
+          idToServerMap.set(id, resultDefault);
+          idToServerMap.get(id).interact(interaction);
+        });
+      }
+    });
+  } else {
+    idToServerMap.get(id).interact(interaction);
+  }
 }
 
 exports.createDefault = createDefault;
